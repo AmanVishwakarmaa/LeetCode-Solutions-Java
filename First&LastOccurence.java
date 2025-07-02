167b06934ee1ffef9742acacab174e9924354895
Aman Vishwakarma <135045842+AmanVishwakarmaa@users.noreply.github.com>
2025-07-02 10:59:15 +0530

    Add solution: First & Last Occurence

---
+++ First&LastOccurence.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = binarySearch(nums, target, true);
+        int last = binarySearch(nums, target, false);
+        return new int[] { first, last };
+    }
+
+    private int binarySearch(int[] nums, int target, boolean findFirst) {
+        int result = -1;
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;
+                if (findFirst) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
